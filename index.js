0ed347eeb084591501c010e485470f8664a4b723
Salu77 <achusalu1416@gmail.com>
2023-10-14 19:43:17 +0530

    doctor-server

---
+++ index.js
@@ -0,0 +1,30 @@
+// 1 import json-server
+
+const jsonserver=require('json-server')
+
+// 2 Create a server application using jsonserver
+
+const server=jsonserver.create()
+
+// 3 Setup path for db.json file
+
+const router=jsonserver.router('db.json')
+
+// 4 Return a middleware used by the jsonserver
+
+const middleware=jsonserver.defaults()
+
+// 5 Setup a port number
+
+const port=process.env.PORT || 3001
+
+// 6 Use in server
+
+server.use(middleware)
+server.use(router)
+
+// 7 To run the port
+
+server.listen(port,()=>{
+    console.log('listening on port '+port);
+})
